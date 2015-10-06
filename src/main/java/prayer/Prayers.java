e9713c4141dac14a5be1323a4f8aec11bb65ed1c
tterrag1098 <tterrag1098@gmail.com>
2015-10-05 22:37:35 -0400

    Update buildscript and fix version replacement

--- src/main/java/prayer/Prayers.java
+++ src/main/java/prayer/Prayers.java
@@ -27,7 +27,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class Prayers
 {
     public static final String MODID = "Prayers";
-    public static final String VERSION = "0.1.4";
+    public static final String VERSION = "@VERSION@";
 
     @Mod.Instance("Prayers")
     public static Prayers instance;
